00a470833bb680c9847faac9efab0b60484cead2
Bikas shah <91305210+Bikas-shah@users.noreply.github.com>
2021-10-04 10:03:03 +0545

    Update allroutes.js

--- routes/allroutes.js
+++ routes/allroutes.js
@@ -1,1 +1,31 @@
 
+var express = require("express");
+var router = express.Router();
+const books = require("../services/books");
+
+//############### GET #################
+router.get('/allbooks',async function(req,res){
+    let allbooks = books.getBook();
+    res.json(allbooks);
+});
+
+//############### POST #################
+router.post('/add',async (req, res) => {
+        let addedBook = books.addBook(req);
+        res.json(addedBook);
+        
+});
+
+//############### PUT #################
+router.put('/update/:id',function(req,res){
+    let updateBooks = books.updateBook(req);
+    res.json(updateBooks);
+});
+
+//############### DELETE #################
+router.delete('/delete/:id',function(req,res){
+    let deleteBooks = books.deleteBook(req);
+    res.json(deleteBooks);
+});
+
+module.exports = router;
